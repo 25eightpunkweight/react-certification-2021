294c08937b750cb4dc1e024f01149f28e4c8c0d0
David Lucero <dvlucero85@gmail.com>
2021-06-22 15:54:25 +0800

    help

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import CardItem from '../../components/CardItem';
 import LoadingSpinner from '../../components/LoadingSpinner';
-import mockData from '../../mock/youtube-mock-data-2.json';
+import mockData from '../../mock/youtube-videos-mock.json.json';
 import { SearchContext } from '../../contexts/SearchContextProvider';
 
 const HomePageWrapper = styled.section`
